f1a9e85ca6af6820afa1d95a30e59b2cb2ae4a49
silc-o <bhussienaldondie@gmail.com>
2025-11-20 14:22:45 +0800

    transfered loginregister.vue changes to loginpage.vue

--- src/pages/Auth/LoginRegister.vue
+++ src/pages/Auth/LoginRegister.vue
@@ -126,7 +126,6 @@ const authStore = useAuthStore();
 const isLogin = ref(true);
 const password = ref('');
 const confirmPassword = ref('');
-const selectedRole = ref<'student' | 'officer' | 'admin'>('student');
 const passwordError = ref('');
 const firstName = ref('');
 const lastName = ref('');
